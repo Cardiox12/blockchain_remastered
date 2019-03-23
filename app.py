24c553be2118b5b7993be3f586671c04b003eef8
Cardiox12 <cardiox1212@hotmail.fr>
2019-03-23 23:30:44 +0100

    app now manage 404 error successfuly

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from core.blockchain import Blockchain
 
 app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
+
 blockchain = Blockchain()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -13,6 +15,10 @@ def home_page():
 @app.route('/about')
 def about():
     return render_template('pages/about.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('pages/404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
